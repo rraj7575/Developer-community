9772ceb90f5edbbf8f11b735ded1568def07b502
rohit <rohit@gigzoe.com>
2022-02-21 10:28:32 +0530

    removed dev tool extension

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -5,18 +5,18 @@ import logger from 'redux-logger'
 const middleware = [thunk, logger]
 const initialState = {}
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(...middleware)
-  )
-)
-
 // const store = createStore(
 //   rootReducer,
 //   initialState,
-//   applyMiddleware(...middleware)
+//   compose(
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+//     applyMiddleware(...middleware)
+//   )
 // )
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  applyMiddleware(...middleware)
+)
  export default store
