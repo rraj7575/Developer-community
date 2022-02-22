d367f5272a270e9bbeecfff6cfaa0a60a97b160b
rohit <rohit@gigzoe.com>
2022-02-22 09:29:42 +0530

    removed commented code inside App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,6 @@ import Loadable from 'react-loadable'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Landing from './components/layout/Landing'
-// import Register from './components/auth/Register'
-// import Login from './components/auth/Login'
-// import Dashboard from './components/dashboard/Dashboard'
 import {Provider} from 'react-redux'
 import jwt_decode from 'jwt-decode'
 import setAuthToken from './utils/setAuthToken'
@@ -15,15 +12,6 @@ import {setCurrentUser} from './actions/authActions'
 import store from './store/store'
 import {clearCurrentProfile} from "./actions/profileAction";
 import PrivateRoute from './components/common/PrivateRoute'
-// import CreateProfile from './components/create-profile/CreateProfile'
-// import EditProfile from './components/edit-profile/EditProfile'
-// import AddExperience from './components/add-credentials/AddExperience'
-// import AddEducation from './components/add-credentials/AddEducation'
-// import Profiles from './components/profiles/Profiles'
-// import Profile from './components/profile/Profile'
-// import Posts from './components/posts/Posts'
-// import Post from './components/post/Post'
-// import Chat from './components/chat/Chat'
 import {doLoad} from "./utils/LoadingComponent";
 
 if (localStorage.jwtToken) {
@@ -50,11 +38,6 @@ class App extends Component {
             <Navbar />
             <Route exact path='/' component={Landing} />
             <div className='container'>
-
-              {/*<Route exact path='/register' component={Register} />*/}
-              {/*<Route exact path='/login' component={Login} />*/}
-              {/*<Route exact path='/profiles' component={Profiles} />*/}
-              {/*<Route exact path='/profile/:handle' component={Profile} />*/}
               <Switch>
                 <Route path='/register' exact
                        render={ (props) => <AsyncRegister {...props} /> } />
@@ -69,31 +52,9 @@ class App extends Component {
                 <PrivateRoute path='/edit-profile' component={AsyncEditProfile} />
                 <PrivateRoute path='/add-experience' component={AsyncAddExperience} />
                 <PrivateRoute path='/add-education' component={AsyncAddEducation} />
-                {/*<PrivateRoute path='/chat' component={AsyncChat} />*/}
                 <PrivateRoute path='/feed' component={AsyncPosts} />
                 <PrivateRoute path='/post/:id' component={AsyncPost} />
               </Switch>
-              {/*<Switch>*/}
-                {/*/!*<PrivateRoute path='/create-profile' component={CreateProfile} />*!/*/}
-              {/*</Switch>*/}
-              {/*<Switch>*/}
-                {/*/!*<PrivateRoute path='/edit-profile' component={EditProfile} />*!/*/}
-              {/*</Switch>*/}
-              {/*<Switch>*/}
-                {/*/!*<PrivateRoute path='/add-experience' component={AddExperience} />*!/*/}
-              {/*</Switch>*/}
-              {/*<Switch>*/}
-                {/*/!*<PrivateRoute path='/add-education' component={AddEducation} />*!/*/}
-              {/*</Switch>*/}
-              {/*<Switch>*/}
-                {/*/!*<PrivateRoute path='/feed' component={Posts} />*!/*/}
-              {/*</Switch>*/}
-              {/*<Switch>*/}
-                {/*/!*<PrivateRoute path='/chat' user={store.auth} component={Chat} />*!/*/}
-              {/*</Switch>*/}
-              {/*<Switch>*/}
-                {/*/!*<PrivateRoute path='/post/:id' component={Post} />*!/*/}
-              {/*</Switch>*/}
             </div>
             <Footer/>
           </div>
@@ -114,7 +75,6 @@ const AsyncAddEducation = Loadable(doLoad(() => import('./components/add-credent
 const AsyncPosts = Loadable(doLoad(() => import('./components/posts/Posts')))
 const AsyncPost = Loadable(doLoad(() => import('./components/post/Post')))
 // const AsyncChat = Loadable(doLoad(() => import('./components/chat/Chat')))
-
 
 
 export default App;
