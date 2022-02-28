bb5cf26d12f45d7b841cb7183ad79de752337710
rohit <rohit@gigzoe.com>
2022-02-28 17:07:46 +0530

    changed port

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const bodyParser = require('body-parser')
 const passport = require('passport')
 const path = require('path')
 const app = express()
-const port = process.env.PORT || 5000;
-const server = app.listen(port, () => console.log(`Server running on port ${port}`))
+const port = process.env.PORT || 6000;
+app.listen(port, () => console.log(`Server running on port ${port}`))
 // const client = require('socket.io').listen(server);
 const Chat = require('./models/Chat')
 const Post = require('./models/Post')
